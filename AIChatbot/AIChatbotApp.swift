9836c0c9774d1349391d74d6a97566889fb3020f
Desmond Fitch <desmond.fitch@icloud.com>
2025-02-10 21:40:03 -0500

    Initial Commit

---
+++ AIChatbot/AIChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatbotApp.swift
+//  AIChatbot
+//
+//  Created by Desmond Fitch on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatbotTests/AIChatbotTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatbotTests.swift
+//  AIChatbotTests
+//
+//  Created by Desmond Fitch on 2/10/25.
+//
+
+import Testing
+@testable import AIChatbot
+
+struct AIChatbotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
