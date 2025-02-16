fad05a2ecc497f5ba122e153f94b4458abda4103
Desmond Fitch <desmond.fitch@icloud.com>
2025-02-15 21:51:20 -0500

    Welcome View Condensing

--- AIChatbot/Core/Welcome/WelcomeView.swift
+++ AIChatbot/Core/Welcome/WelcomeView.swift
@@ -16,43 +16,55 @@ struct WelcomeView: View {
                 ImageLoaderView(urlString: imageName)
                     .ignoresSafeArea()
                 
-                VStack {
-                    Text("AI Chat 🤖")
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                    
-                    Text("Twitter: @ devsmond")
-                        .font(.caption)
-                        .foregroundStyle(.secondary)
-                }
-                .padding(.top)
+                titleSection
+                    .padding(.top)
                 
-                VStack(spacing: 8) {
-                    NavigationLink {
-                        OnboardingCompletedView()
-                    } label: {
-                        Text("Get Started")
-                            .callToActionButton()
-                    }
-                    
-                    Text("Already have an account? Sign in.")
-                        .underline()
-                        .padding(8)
-                        .tappableBackground()
-                }
-                .padding()
+                ctaButtons
+                    .padding()
                 
-                HStack(spacing: 8) {
-                    Link(destination: URL(string: Constants.termsOfServiceURL)!) {
-                        Text("Terms of Service")
-                    }
-                    Circle()
-                        .fill(.accent)
-                        .frame(width: 4)
-                    Link(destination: URL(string: Constants.privacyPolicyURL)!) {
-                        Text("Privacy Policy")
-                    }
-                }
+                policyLinks
+            }
+        }
+    }
+    
+    private var titleSection: some View {
+        VStack {
+            Text("AI Chat 🤖")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Text("Twitter: @ devsmond")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+        }
+    }
+    
+    private var ctaButtons: some View  {
+        VStack(spacing: 8) {
+            NavigationLink {
+                OnboardingCompletedView()
+            } label: {
+                Text("Get Started")
+                    .callToActionButton()
+            }
+            
+            Text("Already have an account? Sign in.")
+                .underline()
+                .padding(8)
+                .tappableBackground()
+        }
+    }
+    
+    private var policyLinks: some View {
+        HStack(spacing: 8) {
+            Link(destination: URL(string: Constants.termsOfServiceURL)!) {
+                Text("Terms of Service")
+            }
+            Circle()
+                .fill(.accent)
+                .frame(width: 4)
+            Link(destination: URL(string: Constants.privacyPolicyURL)!) {
+                Text("Privacy Policy")
             }
         }
     }
