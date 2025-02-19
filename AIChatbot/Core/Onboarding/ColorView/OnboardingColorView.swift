c083ab80a5007608859042cf3eeb1ed9744269e9
Desmond Fitch <desmond.fitch@icloud.com>
2025-02-18 21:09:57 -0500

    [Feature] Onboarding Intro & Color Views

---
+++ AIChatbot/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,72 @@
+//
+//  OnboardingColorView.swift
+//  AIChatbot
+//
+//  Created by Desmond Fitch on 2/18/25.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    @State private var selectedColor: Color?
+    let profileColors: [Color] = [.red, .green, .indigo, .blue, .orange, .pink, .yellow, .purple, .cyan]
+    
+    var body: some View {
+        ScrollView {
+            colorGrid
+                .padding(.horizontal)
+        }
+        .safeAreaInset(edge: .bottom, alignment: .center, spacing: 16, content: {
+            ZStack {
+                if let selectedColor {
+                    ctaButton
+                    .transition(AnyTransition.move(edge: .bottom))
+                }
+            }
+            .padding(24)
+            .background(Color(UIColor.systemBackground))
+        })
+        .animation(.bouncy, value: selectedColor)
+    }
+    
+    private var colorGrid: some View {
+        LazyVGrid(
+            columns: Array(repeating: GridItem(.flexible(), spacing: 16), count: 3),
+            alignment: .center,
+            spacing: 16,
+            pinnedViews: [.sectionHeaders]) {
+                Section {
+                    ForEach(profileColors, id: \.self) { color in
+                        Circle()
+                            .fill(.accent)
+                            .overlay {
+                                color
+                                    .clipShape(Circle())
+                                    .padding(selectedColor == color ? 10 : 0)
+                            }
+                            .onTapGesture {
+                                selectedColor = color
+                            }
+                    }
+                } header: {
+                    Text("Select a profile color")
+                        .font(.headline)
+                }
+            }
+    }
+    
+    private var ctaButton: some View {
+        NavigationLink {
+            OnboardingCompletedView()
+        } label: {
+            Text("Continue")
+                .callToActionButton()
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        OnboardingColorView()
+    }
+}
---
+++ AIChatbot/Core/Onboarding/Intro View/OnboardingIntroView.swift
@@ -0,0 +1,56 @@
+//
+//  OnboardingIntroView.swift
+//  AIChatbot
+//
+//  Created by Desmond Fitch on 2/18/25.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        VStack {
+            Group {
+                Text("Make your own ")
+                +
+                Text("Avatars ")
+                    .foregroundStyle(.accent)
+                    .fontWeight(.semibold)
+                +
+                Text("and chat with them!\n\nHave ")
+                +
+                Text("real conversations")
+                    .foregroundStyle(.accent)
+                    .fontWeight(.semibold)
+                +
+                Text("with AI generated responses.")
+            }
+            .baselineOffset(6)
+            .frame(maxHeight: .infinity)
+            .padding(24)
+            
+            ctaButton
+        }
+        .font(.title3)
+        
+    }
+    
+    private var ctaButton: some View  {
+        VStack(spacing: 8) {
+            NavigationLink {
+                OnboardingColorView()
+            } label: {
+                Text("Continue")
+                    .callToActionButton()
+            }
+            .padding(24)
+            .font(.title3)
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        OnboardingIntroView()
+    }
+}
