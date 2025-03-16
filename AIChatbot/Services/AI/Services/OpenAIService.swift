7682733d7af5f80bb4a73847fc5f970e5ad3fdb4
Desmond Fitch <desmond.fitch@icloud.com>
2025-03-16 11:47:36 -0400

    [Feature] Add generateOpenAIImage, remove OpenAI SPM

--- AIChatbot/Services/AI/Services/OpenAIService.swift
+++ AIChatbot/Services/AI/Services/OpenAIService.swift
@@ -6,36 +6,19 @@
 //
 
 import SwiftUI
-import OpenAI
 import IdentifiableByString
 import FirebaseFunctions
 
-private typealias ChatCompletion = ChatQuery.ChatCompletionMessageParam
-private typealias SystemMessage = ChatQuery.ChatCompletionMessageParam.SystemMessageParam
-private typealias UserMessage = ChatQuery.ChatCompletionMessageParam.UserMessageParam
-private typealias UserTextContent = ChatQuery.ChatCompletionMessageParam.UserMessageParam.Content
-private typealias AssistantMessage = ChatQuery.ChatCompletionMessageParam.AssistantMessageParam
-
 struct OpenAIService: AIService {
-    var openAI: OpenAI {
-        OpenAI(apiToken: Keys.openAI)
-    }
     func generateImage(input: String) async throws -> UIImage {
-        let query = ImagesQuery(
-            prompt: input,
-            n: 1,
-            quality: .hd,
-            responseFormat: .b64_json,
-            size: ._512,
-            style: .natural,
-            user: nil
-        )
+        let response = try await Functions.functions().httpsCallable("generateOpenAIImage").call([
+            "input": input
+        ])
         
-        let result = try await openAI.images(query: query)
-        
-        guard let b64Json = result.data.first?.b64Json,
-              let data = Data(base64Encoded: b64Json),
-              let image = UIImage(data: data) else {
+        guard
+            let b64Json = response.data as? String,
+            let data = Data(base64Encoded: b64Json),
+            let image = UIImage(data: data) else {
             throw OpenAIError.invalidResponse
         }
         
@@ -83,15 +66,6 @@ struct AIChatModel: Codable {
         self.message = content
     }
     
-    init?(chat: ChatResult.Choice.ChatCompletionMessage) {
-        self.role = AIChatRole(role: chat.role)
-        if let string = chat.content?.string {
-            self.message = string
-        } else {
-            return nil
-        }
-    }
-    
     var eventParameters: [String: Any] {
         let dict: [String: Any?] = [
             "aiChat_\(CodingKeys.role.rawValue)": role.rawValue,
@@ -105,49 +79,8 @@ struct AIChatModel: Codable {
         case role
         case message
     }
-    
-    fileprivate func toOpenAIModel() -> ChatCompletion? {
-        switch role {
-        case .system:
-            return ChatCompletion.system(SystemMessage(content: message))
-        case .user:
-            return ChatCompletion.user(UserMessage(content: ChatQuery.ChatCompletionMessageParam.UserMessageParam.Content(string: message)))
-        case .assistant:
-            return ChatCompletion.assistant(AssistantMessage(content: message))
-        case .tool:
-            return nil
-        }
-    }
-}
+ }
 
 enum AIChatRole: String, Codable {
     case system, user, assistant, tool
-    
-    init(role: ChatQuery.ChatCompletionMessageParam.Role) {
-        switch role {
-        case .system:
-            self = .system
-        case .user:
-            self = .user
-        case .assistant:
-            self = .assistant
-        case .tool:
-            self = .tool
-        default:
-            self = .tool
-        }
-    }
-    
-    var openAIRole: ChatQuery.ChatCompletionMessageParam.Role {
-        switch self {
-        case .system:
-            return .system
-        case .user:
-            return .user
-        case .assistant:
-            return .assistant
-        case .tool:
-            return .tool
-        }
-    }
 }
